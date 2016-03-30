c79442a6b551723a128e5505eb6a4d20d774b375
Javier Enrique <s0wkete@gmail.com>
2016-03-30 18:26:57 -0300

    defining routes of the app

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,12 +1,37 @@
 export function routerConfig ($stateProvider, $urlRouterProvider) {
   'ngInject';
   $stateProvider
-    .state('home', {
+
+    .state('demo', {
       url: '/example',
       templateUrl: 'app/main/main.html',
-      controller: 'MainController',
+      controller: 'DemoController',
       controllerAs: 'main'
+    })
+
+    .state('home', {
+      abstract: true,
+      templateUrl: 'app/map/main.html',
+      controller: function(){console.log('main')},
+      controllerAs: 'main',
+    })
+
+    .state('home.map', {
+      url: '/',
+      views: {
+        'sidebar': {
+          template: 'sidebar',
+        },
+        'container': {
+          template: 'map container',
+        }
+      }
     });
 
-  $urlRouterProvider.otherwise('/example');
+    // .sate('home.about', {
+    //   url: '/about',
+    //   onEnter: function(){}
+    // });
+
+  $urlRouterProvider.otherwise('/');
 }
