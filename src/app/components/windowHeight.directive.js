bea922f08060a3d61dada1c62ab04b4fe1e8f05c
Javier Enrique <s0wkete@gmail.com>
2016-03-31 15:46:49 -0300

    added window height directive

---
+++ src/app/components/windowHeight.directive.js
@@ -0,0 +1,25 @@
+/* global _:false */
+export function windowHeight($window, $log) {
+  'ngInject'
+
+  return function link(scope, element) {
+
+    var windowContent = angular.element($window);
+
+    $log.debug('asdf');
+
+    function setHeight() {
+      var windowHeight = $window.innerHeight;
+      element.css('height', windowHeight + 'px');
+    }
+
+    // init's
+    setHeight();
+
+    // height in resize
+    windowContent.on('resize', _.debounce(setHeight, 250));
+    windowContent.on('$destroy', setHeight());
+
+  }
+
+}
